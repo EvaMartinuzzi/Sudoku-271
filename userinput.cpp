0c96e8c1b9b839665621685f139c75b548ab5a4a
Eva Martinuzzi <eva.martinuzzi@sportngin.com>
2016-12-07 02:44:23 +0000

    Create userinput.cpp

---
+++ userinput.cpp
@@ -0,0 +1,18 @@
+#include<string>
+#include<cstring>
+#include<iostream>
+
+using namespace std;
+
+void userinput(int row, int col, int m, int **a) {
+	int i, j;
+	cout << "Enter the row you would like: " << endl;
+	cin >> row;
+	cout << "Enter the column you would like: " << endl;
+	cin >> col;
+	cout << "What value would you like to enter?" << endl;
+	cin >> m;
+
+	a[row][col];
+
+}
